fc1bd339f1c48e301e4218ab70d397d6c9c43cef
alexliu2360 <alexliu2360@126.com>
2018-12-06 20:19:55 +0800

    Update base_features.py

--- base_features.py
+++ base_features.py
@@ -15,22 +15,44 @@ def load_data():
 
 
 def fetch_op_fts(op_train):
-    op_train_gb = op_train.groupby('UID')
-    day_cnts = op_train_gb['day'].count()
-    top_appear_day = op_train_gb['day'].value_counts().sort_values()
+    # op_train_gb = op_train.groupby('UID')
+    # day_cnts = op_train_gb['day'].count()
+    # top_appear_day = op_train_gb['day'].value_counts().sort_values()
+    uids = []
+    features = []
+    for index, row in op_train.iterrows():
+        if index / 100 == 20:
+            break
+        if index % 100 == 0:
+            print(index)
+        if row['UID'] in uids:
+            continue
+        else:
+            uids.append(row['UID'])
+        feature = {}
+        feature['UID'] = row['UID']
+        tmp = op_train[op_train['UID'] == row['UID']]
+        feature['day_cnts'] = tmp['day'].count()
+        feature['top_appear_day'] = tmp['day'].value_counts()
+        features.append(feature)
+    features = pd.DataFrame(features)
+    return features
 
 
 def fetch_tran_fts(tran_train):
-    tran_train
+    # tran_train
+    pass
 
 
 def fetch_features(op_train, tran_train):
     fetch_op_fts(op_train)
     fetch_tran_fts(tran_train)
 
+
 def main():
     op_train, tran_train, tag_train = load_data()
     fetch_features(op_train, tran_train)
+
 
 if __name__ == '__main__':
     main()
