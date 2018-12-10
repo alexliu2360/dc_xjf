e6a83b486b064e529e8e62eb8256a7e0a07db646
alexliu <alexliu2360@126.com>
2018-12-10 14:46:53 +0800

    提升

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
 import lightgbm as lgb
 import pandas as pd
 import config as cfg
@@ -29,60 +30,46 @@ def load_train_test_data():
     test_data = pd.read_csv(cfg.round1_fts_file)
     return train_data, test_data
 
-def fillna(data):
-    data.fillna(-1, inplace=True)
-    return data
-
-
-def LGB_test(train_x, train_y, test_x, test_y):
-    print("LGB test")
-    clf = lgb.LGBMClassifier(
-        boosting_type='gbdt', num_leaves=31, reg_alpha=0.0, reg_lambda=1,
-        max_depth=-1, n_estimators=3000, objective='binary',
-        subsample=0.7, colsample_bytree=0.7, subsample_freq=1,  # colsample_bylevel=0.7,
-        learning_rate=0.01, min_child_weight=25, random_state=2018, n_jobs=50
-    )
-    clf.fit(train_x, train_y, eval_set=[(train_x, train_y), (test_x, test_y)], early_stopping_rounds=100)
-    feature_importances = sorted(zip(train_x.columns, clf.feature_importances_), key=lambda x: x[1])
-    return clf.best_score_['valid_1']['binary_logloss'], feature_importances
-
-
-def off_test_split(data, cate_col=None):
-    y = data.pop('Tag')
-    train_x, test_x, train_y, test_y = train_test_split(data, y, test_size=0.15, random_state=2018)
-    score = LGB_test(train_x, train_y, test_x, test_y)
-    return score[1]
-
-
-def LGB_predict(data, file):
-    train = data[data['is_trade'] > -1]
-    predict = data[data['is_trade'] == -2]
-    res = predict[['instance_id']]
-    train_y = train.pop('is_trade')
-    train_x = train.drop(['day', 'instance_id'], axis=1)
-    test_x = predict.drop(['day', 'instance_id', 'is_trade'], axis=1)
-    clf = lgb.LGBMClassifier(
-        boosting_type='gbdt', num_leaves=31, reg_alpha=0.0, reg_lambda=1,
-        max_depth=-1, n_estimators=3000, objective='binary',
-        subsample=0.7, colsample_bytree=0.7, subsample_freq=1,  # colsample_bylevel=0.7,
-        learning_rate=0.01, min_child_weight=25, random_state=2018, n_jobs=50
-    )
-    clf.fit(train_x, train_y, eval_set=[(train_x, train_y)])
-    res['predicted_score'] = clf.predict_proba(test_x)[:, 1]
-    testb = pd.read_csv('../data/round2_ijcai_18_test_b_20180510.txt', sep=' ')[['instance_id']]
-    res = pd.merge(testb, res, on='instance_id', how='left')
-    res[['instance_id', 'predicted_score']].to_csv('../submit/' + file + '.txt', sep=' ', index=False)
-
-
-def add(f1, f2):
-    for i in f2:
-        f1 = pd.merge(f1, i, on='instance_id', how='left')
-    return f1
-
 
 if __name__ == '__main__':
     train_data, test_data = load_train_test_data()
-    score = off_test_split(train_data)
-    add_feature = [i[0] for i in score[-cross_feature_num:]]
-    base_add = pd.concat([base, cross[add_feature]], axis=1)
-    LGB_predict(base_add, 'bryan_submit')
+    # 引入train test数据
+    train_data, test_data = load_train_test_data()
+    train_data.drop(['Unnamed: 0'], axis=1, inplace=True)
+    test_data.drop(['Unnamed: 0'], axis=1, inplace=True)
+    train_data.fillna(-1, inplace=True)
+    test_data.fillna(-1, inplace=True)
+
+    label = train_data['Tag']
+    train = train_data.drop(['UID', 'Tag'], axis=1)
+    test_id = test_data['UID']
+    test = test_data.drop(['UID', 'Tag'], axis=1)
+
+    lgb_model = lgb.LGBMClassifier(boosting_type='gbdt', num_leaves=100, reg_alpha=0.0, reg_lambda=1, max_depth=-1,
+                                   n_estimators=5000, objective='binary', subsample=0.7, colsample_bytree=0.77,
+                                   subsample_freq=1, learning_rate=0.01,
+                                   random_state=2018, n_jobs=50, min_child_weight=4, min_child_samples=5,
+                                   min_split_gain=0)
+    skf = StratifiedKFold(n_splits=5, random_state=2018, shuffle=True)
+    best_score = []
+
+    oof_preds = np.zeros(train.shape[0])
+    sub_preds = np.zeros(test_id.shape[0])
+
+    for index, (train_index, test_index) in enumerate(skf.split(train, label)):
+        lgb_model.fit(train.iloc[train_index], label.iloc[train_index], verbose=50,
+                      eval_set=[(train.iloc[train_index], label.iloc[train_index]),
+                                (train.iloc[test_index], label.iloc[test_index])], early_stopping_rounds=50)
+        best_score.append(lgb_model.best_score_['valid_1']['binary_logloss'])
+        print(best_score)
+        oof_preds[test_index] = lgb_model.predict_proba(train.iloc[test_index],
+                                                        num_iteration=lgb_model.best_iteration_)[:, 1]
+
+        test_pred = lgb_model.predict_proba(test, num_iteration=lgb_model.best_iteration_)[:, 1]
+        sub_preds += test_pred / 5
+
+    m = tpr_weight_funtion(y_predict=oof_preds, y_true=label)
+    print(m[1])
+    submit = pd.read_csv('../data/submit_sample.csv')
+    submit['Tag'] = sub_preds
+    submit.to_csv('../data/sub/baseline_%s.csv' % str(m), index=False)
